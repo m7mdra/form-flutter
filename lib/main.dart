3245bae8f2ee163bf7a2a83dad9c79a658cd16e6
m7mdra <xm7mdrax@gmail.com>
2023-03-13 11:43:38 +0300

    more views

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'dart:io';
 
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:video_player/video_player.dart';
 import 'package:video_thumbnail/video_thumbnail.dart';
 
-
 void main() {
-  runApp(const MyApp());
+  runApp(DevicePreview(
+    builder: (BuildContext context) {
+      return const MyApp();
+    },
+    enabled: true,
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -44,23 +49,25 @@ class _MyHomePageState extends State<MyHomePage> {
         id: '1',
         hint: 'Please write you answer here.',
         title:
-        'Breaking: Changed resourcePrefix to maplibre_ from mapbox_ 647 and renamed resources accordingly. Note that this is a breaking change since the names of public resources were renamed as well. Replaced Mapbox logo with MapLibre logo.'));
+            'Breaking: Changed resourcePrefix to maplibre_ from mapbox_ 647 and renamed resources accordingly. Note that this is a breaking change since the names of public resources were renamed as well. Replaced Mapbox logo with MapLibre logo.'));
 
     list.add(ImageQuestion(
         id: '2',
         value: [],
         title:
-        'GMS location: Replace new LocationRequest() with LocationRequest.Builder, and LocationRequest.PRIORITY_X with Priority.PRIORITY_X'));
+            'GMS location: Replace new LocationRequest() with LocationRequest.Builder, and LocationRequest.PRIORITY_X with Priority.PRIORITY_X'));
     list.add(VideoQuestion(
         id: '3',
         title:
-        'Increment minSdkVersion from 14 to 21, as it covers 99.2%% of the newer devices since 2014'));
-    list.add(MultiSelectionQuestion(
-        ['Option1', 'Option2', 'Option3', 'Option4'],
-        id: '4', title: 'and lessens the backward compatibility burden (630)'));
+            'Increment minSdkVersion from 14 to 21, as it covers 99.2%% of the newer devices since 2014'));
     list.add(SingleSelectionQuestion(
         ['Option1', 'Option2', 'Option3', 'Option4'],
         id: '5', title: 'Catches NaN for onMove event (621)'));
+    list.add(MultiSelectionQuestion(
+        ['Option1', 'Option2', 'Option3', 'Option4'],
+        value: [],
+        id: '4',
+        title: 'and lessens the backward compatibility burden (630)'));
   }
 
   @override
@@ -70,30 +77,103 @@ class _MyHomePageState extends State<MyHomePage> {
         child: ListView.separated(
           padding: const EdgeInsetsDirectional.symmetric(horizontal: 16),
           itemBuilder: (context, index) {
-            var question = list[index];
-            switch (question.type) {
+            var q = list[index];
+            switch (q.type) {
               case QuestionType.input:
-                return InputWidget(question: question as InputQuestion);
+                return InputWidget(question: q as InputQuestion);
               case QuestionType.image:
-                return ImageQuestionWidget(question: question as ImageQuestion);
+                return ImageQuestionWidget(question: q as ImageQuestion);
               case QuestionType.singleSelection:
-                return ListTile(title: Text(question.title));
+                return SingleSelectionQuestionWidget(
+                    question: q as SingleSelectionQuestion);
               case QuestionType.multiSelection:
-                return ListTile(title: Text(question.title));
+                return MultipleSelectionQuestionWidget(
+                    question: q as MultiSelectionQuestion);
               case QuestionType.video:
-                return VideoQuestionWidget(question: question as VideoQuestion);
+                return VideoQuestionWidget(question: q as VideoQuestion);
               case QuestionType.audio:
                 return Container();
               case QuestionType.title:
-                return SectionTitleWidget(question: question as TitleQuestion);
+                return SectionTitleWidget(question: q as TitleQuestion);
             }
           },
           itemCount: list.length,
           separatorBuilder: (BuildContext context, int index) =>
-          const Divider(),
+              const Divider(),
         ),
       ),
     );
+  }
+}
+
+class SingleSelectionQuestionWidget extends StatelessWidget {
+  final SingleSelectionQuestion question;
+
+  const SingleSelectionQuestionWidget({Key? key, required this.question})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+        valueListenable: question,
+        builder: (context, value, _) {
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              QuestionTitleWidget(question: question),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: question.items
+                    .map((e) => RadioListTile(
+                          title: Text(e),
+                          value: e,
+                          groupValue: value,
+                          onChanged: (newValue) {
+                            question.value = newValue;
+                          },
+                        ))
+                    .toList(),
+              ),
+            ],
+          );
+        });
+  }
+}
+
+class MultipleSelectionQuestionWidget extends StatelessWidget {
+  final MultiSelectionQuestion question;
+
+  const MultipleSelectionQuestionWidget({Key? key, required this.question})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+        valueListenable: question,
+        builder: (context, value, _) {
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              QuestionTitleWidget(question: question),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: question.items
+                    .map((e) => CheckboxListTile(
+                          title: Text(e),
+                          value: value?.contains(e) == true,
+                          onChanged: (newValue) {
+                            if (newValue == true) {
+                              question.add(e);
+                            } else {
+                              question.remove(e);
+                            }
+                          },
+                        ))
+                    .toList(),
+              ),
+            ],
+          );
+        });
   }
 }
 
@@ -285,7 +365,7 @@ class InputWidget extends StatelessWidget {
           decoration: InputDecoration(
               hintText: question.hint,
               border:
-              OutlineInputBorder(borderRadius: BorderRadius.circular(4)),
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(4)),
               isDense: true),
         )
       ],
@@ -331,15 +411,15 @@ abstract class Question<T> extends ValueNotifier<T> {
 
   Question(
       {required this.id,
-        required this.title,
-        required T value,
-        required this.type})
+      required this.title,
+      required T value,
+      required this.type})
       : super(value);
 
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-          other is Question && runtimeType == other.runtimeType && id == other.id;
+      other is Question && runtimeType == other.runtimeType && id == other.id;
 
   @override
   int get hashCode => id.hashCode;
@@ -383,25 +463,33 @@ class SingleSelectionQuestion extends Question<String?> {
       {required String id, required String title, String? value})
       : assert(items.isNotEmpty),
         super(
-          id: id,
-          title: title,
-          value: value,
-          type: QuestionType.singleSelection);
+            id: id,
+            title: title,
+            value: value,
+            type: QuestionType.singleSelection);
 }
 
 class MultiSelectionQuestion extends Question<List<String>?> {
   final List<String> items;
 
   MultiSelectionQuestion(this.items,
-      {required String id,
-        required String title,
-        List<String>? value = const []})
+      {required String id, required String title, required List<String>? value})
       : assert(items.isNotEmpty),
         super(
-          id: id,
-          title: title,
-          value: value,
-          type: QuestionType.multiSelection);
+            id: id,
+            title: title,
+            value: value,
+            type: QuestionType.multiSelection);
+
+  void add(String newValue) {
+    value?.add(newValue);
+    notifyListeners();
+  }
+
+  void remove(String newValue) {
+    value?.remove(newValue);
+    notifyListeners();
+  }
 }
 
 class ImageQuestion extends Question<List<String>> {
@@ -410,10 +498,10 @@ class ImageQuestion extends Question<List<String>> {
 
   ImageQuestion(
       {required String id,
-        this.max = 1,
-        this.min = 1,
-        required String title,
-        List<String> value = const []})
+      this.max = 1,
+      this.min = 1,
+      required String title,
+      List<String> value = const []})
       : assert(max >= min),
         super(id: id, title: title, value: value, type: QuestionType.image);
 
@@ -467,7 +555,7 @@ class _MediaPickerModelState extends State<MediaPickerModel> {
             onTap: () async {
               try {
                 var result =
-                await imagePicker.pickImage(source: ImageSource.camera);
+                    await imagePicker.pickImage(source: ImageSource.camera);
                 if (mounted) {
                   Navigator.maybePop(context, result);
                 }
@@ -484,7 +572,7 @@ class _MediaPickerModelState extends State<MediaPickerModel> {
             onTap: () async {
               try {
                 var result =
-                await imagePicker.pickVideo(source: ImageSource.gallery);
+                    await imagePicker.pickVideo(source: ImageSource.gallery);
                 if (mounted) {
                   Navigator.maybePop(context, result);
                 }
@@ -501,7 +589,7 @@ class _MediaPickerModelState extends State<MediaPickerModel> {
             onTap: () async {
               try {
                 var result =
-                await imagePicker.pickImage(source: ImageSource.gallery);
+                    await imagePicker.pickImage(source: ImageSource.gallery);
                 if (mounted) {
                   Navigator.maybePop(context, result);
                 }
@@ -518,7 +606,7 @@ class _MediaPickerModelState extends State<MediaPickerModel> {
             onTap: () async {
               try {
                 var result =
-                await imagePicker.pickVideo(source: ImageSource.gallery);
+                    await imagePicker.pickVideo(source: ImageSource.gallery);
                 if (mounted) {
                   Navigator.maybePop(context, result);
                 }
@@ -588,6 +676,12 @@ class _VideoViewerPageState extends State<VideoViewerPage> {
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
         onTap: () {
@@ -608,11 +702,11 @@ class _VideoViewerPageState extends State<VideoViewerPage> {
                       AspectRatio(
                           aspectRatio: _controller.value.aspectRatio,
                           child: VideoPlayer(_controller)),
-
                       VideoProgressIndicator(
                         _controller,
                         allowScrubbing: true,
-                        colors: VideoProgressColors(playedColor: Theme.of(context).colorScheme.primary),
+                        colors: VideoProgressColors(
+                            playedColor: Theme.of(context).colorScheme.primary),
                       ),
                     ],
                   ),
@@ -629,119 +723,3 @@ class _VideoViewerPageState extends State<VideoViewerPage> {
         ));
   }
 }
-class _ControlsOverlay extends StatelessWidget {
-  const _ControlsOverlay({required this.controller});
-
-  static const List<Duration> _exampleCaptionOffsets = <Duration>[
-    Duration(seconds: -10),
-    Duration(seconds: -3),
-    Duration(seconds: -1, milliseconds: -500),
-    Duration(milliseconds: -250),
-    Duration.zero,
-    Duration(milliseconds: 250),
-    Duration(seconds: 1, milliseconds: 500),
-    Duration(seconds: 3),
-    Duration(seconds: 10),
-  ];
-  static const List<double> _examplePlaybackRates = <double>[
-    0.25,
-    0.5,
-    1.0,
-    1.5,
-    2.0,
-    3.0,
-    5.0,
-    10.0,
-  ];
-
-  final VideoPlayerController controller;
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        AnimatedSwitcher(
-          duration: const Duration(milliseconds: 50),
-          reverseDuration: const Duration(milliseconds: 200),
-          child: controller.value.isPlaying
-              ? const SizedBox.shrink()
-              : Container(
-            color: Colors.black26,
-            child: const Center(
-              child: Icon(
-                Icons.play_arrow,
-                color: Colors.white,
-                size: 100.0,
-                semanticLabel: 'Play',
-              ),
-            ),
-          ),
-        ),
-        GestureDetector(
-          onTap: () {
-            controller.value.isPlaying ? controller.pause() : controller.play();
-          },
-        ),
-        Align(
-          alignment: Alignment.topLeft,
-          child: PopupMenuButton<Duration>(
-            initialValue: controller.value.captionOffset,
-            tooltip: 'Caption Offset',
-            onSelected: (Duration delay) {
-              controller.setCaptionOffset(delay);
-            },
-            itemBuilder: (BuildContext context) {
-              return <PopupMenuItem<Duration>>[
-                for (final Duration offsetDuration in _exampleCaptionOffsets)
-                  PopupMenuItem<Duration>(
-                    value: offsetDuration,
-                    child: Text('${offsetDuration.inMilliseconds}ms'),
-                  )
-              ];
-            },
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                // Using less vertical padding as the text is also longer
-                // horizontally, so it feels like it would need more spacing
-                // horizontally (matching the aspect ratio of the video).
-                vertical: 12,
-                horizontal: 16,
-              ),
-              child: Text('${controller.value.captionOffset.inMilliseconds}ms'),
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.topRight,
-          child: PopupMenuButton<double>(
-            initialValue: controller.value.playbackSpeed,
-            tooltip: 'Playback speed',
-            onSelected: (double speed) {
-              controller.setPlaybackSpeed(speed);
-            },
-            itemBuilder: (BuildContext context) {
-              return <PopupMenuItem<double>>[
-                for (final double speed in _examplePlaybackRates)
-                  PopupMenuItem<double>(
-                    value: speed,
-                    child: Text('${speed}x'),
-                  )
-              ];
-            },
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                // Using less vertical padding as the text is also longer
-                // horizontally, so it feels like it would need more spacing
-                // horizontally (matching the aspect ratio of the video).
-                vertical: 12,
-                horizontal: 16,
-              ),
-              child: Text('${controller.value.playbackSpeed}x'),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
